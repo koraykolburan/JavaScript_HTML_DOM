3830fdc5d53351c926b2ed2e013c8936591442b9
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-02-14 18:29:25 +0300

    Js HTML DOM Part-4

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ div1.classList.add("content");
 document.body.appendChild(div1);
 document.getElementById("div1")
 
-//creating two ARTICLEs with a class
+//creating two ARTICLEs with a class & writing hmtl dom comment/note
 const article1 = document.createElement("article");
 article1.setAttribute("class", "htmlDom");
 div1.appendChild(article1);
+
+const comment2 = document.createComment("Menu with DOM");
+div1.appendChild(comment2);
+
 const article2 = document.createElement("article");
 article2.setAttribute("class", "menuDOM");
 div1.appendChild(article2);
+
 
 //creating a H1 and P tag
 const head1 = document.createElement("h1");
@@ -32,33 +37,35 @@ article1.appendChild(p1);
 //creating CODE & SPAN & ANCHOR tags
 const c1 = document.createElement("code");
 const t2 = document.createTextNode("\"document.head\"");
-
 const br3 = document.createElement("BR");
-const c5 = document.createElement("code");
-const t9 = document.createTextNode("\"document.body\"");
-
-const br5 = document.createElement("BR");
-const s1 = document.createElement("span");
-const t3 = document.createTextNode(" this is the \"HTMLImageElement\": ");
-
-const br4 = document.createElement("BR");
-const c6 = document.createElement("code");
-const t10 = document.createTextNode("\"document.querySelector(\"figure.(class or id name) > img\");");
-const br6 = document.createElement("BR");
-
-
 article1.appendChild(c1);
 c1.appendChild(t2);
 article1.appendChild(br3);
+
+const c5 = document.createElement("code");
+const t9 = document.createTextNode("\"document.body\"");
+const br4 = document.createElement("BR");
 article1.appendChild(c5);
 c5.appendChild(t9);
 article1.appendChild(br4);
-article1.appendChild(s1);
-s1.appendChild(t3);
+
+const c6 = document.createElement("code");
+const t10 = document.createTextNode("\"document.querySelector(\"figure.(class or id name) > img\");");
 article1.appendChild(c6);
 c6.appendChild(t10);
+
+const s1 = document.createElement("span");
+const t3 = document.createTextNode(" this is the \"HTMLImageElement\". ");
+const br5 = document.createElement("BR");
+const br6 = document.createElement("BR");
+article1.appendChild(s1);
+s1.appendChild(t3);
+document.getElementsByTagName("span")[0].style.textDecoration = "underline";
+document.getElementsByTagName("span")[0].style.color = "pink";
+document.getElementsByTagName("span")[0].style.fontStyle = "italic";
 article1.appendChild(br5);
 article1.appendChild(br6);
+
 
 
 const a1 = document.createElement("A");
@@ -76,6 +83,8 @@ article1.appendChild(p2);
 const hr1 = document.createElement("HR");
 article1.appendChild(hr1);
 
+// article2 --------------------------------------------------------------------------------------------------
+
 const head2 = document.createElement("h2");
 const h2 = document.createTextNode("Let's Create some HTML using JavaScript");
 head2.style.color = "#e8eaea";
@@ -84,35 +93,56 @@ article2.appendChild(head2);
 
 const c2 = document.createElement("code");
 const t6 = document.createTextNode("\"document.createElement(tagName, options)\" ");
-const c3 = document.createElement("code");
-const t7 = document.createTextNode("\"document.createTextNode(\"text\")");
-const c4 = document.createElement("code");
-const t8 = document.createTextNode("\"appendChild()\"");
-const c7 = document.createElement("code");
-const t11 = document.createTextNode("\"getElementsByTag(\"P or H1\")[0]\"");
 const br1 = document.createElement("BR");
-const br2 = document.createElement("BR");
-const br7 = document.createElement("BR");
 c2.appendChild(t6);
 article2.appendChild(c2);
 article2.appendChild(br1);
+
+const c3 = document.createElement("code");
+const t7 = document.createTextNode("\"document.createTextNode(\"text\")");
+const br2 = document.createElement("BR");
 c3.appendChild(t7);
 article2.appendChild(c3);
 article2.appendChild(br2);
+
+const c4 = document.createElement("code");
+const t8 = document.createTextNode("\"appendChild()\"");
+const br7 = document.createElement("BR");
 c4.appendChild(t8);
 article2.appendChild(c4);
 article2.appendChild(br7);
+
+const c7 = document.createElement("code");
+const t11 = document.createTextNode("\"getElementsByTagName(\"P or H1\")[0]\" ");
 c7.appendChild(t11);
 article2.appendChild(c7);
 
-const p3 = document.createElement("p");
-const t12 = document.createTextNode("Remember: the \"getElementsByTagName()[2]\" works with indexes like these; [0],[1],[2],[3] . ");
-p3.appendChild(t12);
-article2.appendChild(p3);
-document.getElementsByTagName("p")[2].style.textDecorationLine = "underline";
-document.getElementsByTagName("p")[2].style.textDecorationStyle = "double";
+const s2 = document.createElement("span");
+const t12 = document.createTextNode("Remember: this tag works efficiently with indexes like these; [0],[1],[2],[3] . ");
+s2.appendChild(t12);
+article2.appendChild(s2);
+document.getElementsByTagName("span")[1].style.textDecoration= "underline";
+document.getElementsByTagName("span")[1].style.color= "pink";
+document.getElementsByTagName("span")[1].style.fontStyle= "italic";
+
+const br8 = document.createElement("BR");
+article2.appendChild(br8);
+
+const c8 = document.createElement("code");
+const t13 = document.createTextNode("\"getElementsByClassName()[1]\" ");
+c8.appendChild(t13);
+article2.appendChild(c8);
+
+const s3 = document.createElement("span");
+const t14 = document.createTextNode("Remember: this tag works efficiently with indexes like these; [0],[1],[2],[3] .");
+s3.appendChild(t14);
+article2.appendChild(s3);
+document.getElementsByTagName("span")[2].style.textDecoration= "underline";
+document.getElementsByTagName("span")[2].style.color= "pink";
+document.getElementsByTagName("span")[2].style.fontStyle= "italic";
 
 // -------------------------------------------------------- Create A Menu with DOM ---------------------------------
+
 const head3 = document.createElement("h3");
 const h3 = document.createTextNode("Create a MENU with DOM & JavaScript");
 head3.style.fontStyle = "italic";
@@ -138,3 +168,4 @@ function createMenu ( items ) {
 }
 // Calling the function finally
 createMenu(technologies);
+//-------------------------------------------------------------------------------------------------
