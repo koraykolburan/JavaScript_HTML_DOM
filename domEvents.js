ff839d7c7f6c3bf05412e7e53a19b6a6ee3c0ff6
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-02-20 22:44:41 +0300

    HTML DOM Events was done.

--- domEvents.js
+++ domEvents.js
@@ -51,3 +51,79 @@ function checkCookies(){
     document.getElementById("cookies").innerHTML = text;
 }
 
+//The oninput Event
+function upperCase(){
+    const x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
+
+//The onchange Event
+function upperCase2(){
+    const x2 = document.getElementById("fname2");
+    x2.value = x2.value.toUpperCase();
+}
+
+//The onmouseover and onmouseout Attribute
+function mOver(obj){
+    obj.innerHTML = "Oops! Thank You!";
+    obj.style.width = "150px";
+    obj.style.height = "50px";
+    obj.style.padding = "40px";
+    obj.style.cursor = "wait";
+    obj.style.textAlign = "center";
+    obj.style.borderRadius = "12px";
+    obj.style.color = "red";
+    obj.style.fontSize = "25px";
+}
+
+function mOveout(obj){
+    obj.innerHTML = "Goodbye!";
+    obj.style.width = "120px";
+    obj.style.height = "20px";
+    obj.style.padding = "40px";
+    obj.style.color = "blue";
+    obj.style.fontSize = "15px";
+}
+
+//The onmousedown & onmouseup attributes
+function mDown(obj){
+    obj.innerHTML = "Wow I'm so much BIGGER right now! Than, Let go of me!";
+    obj.style.backgroundColor = "pink";
+    obj.style.color = "red";
+    obj.style.width = "90%";
+    obj.style.height = "40px";
+    obj.style.padding = "40px";
+    obj.style.fontSize = "22px";
+    obj.style.borderRadius = "12px";
+}
+
+function mUp(obj){
+    obj.innerHTML = "Have a nice day!";
+    obj.style.backgroundColor = "darkcyan";
+    obj.style.color = "black";
+    obj.style.width = "120px";
+    obj.style.height = "40px";  
+    obj.style.padding = "40px";
+}
+
+//DOM Event Listeners
+let y = document.getElementById("myBtn");
+y.addEventListener("mouseover", myFunction3)
+y.addEventListener("click", myFunction4)
+y.addEventListener("mouseout", myFunction5)
+
+function myFunction3(){
+    document.getElementById("demo").innerHTML += "Moused over! <br>";
+}
+function myFunction4(){
+    document.getElementById("demo").innerHTML += "Clicked! <br>";
+}
+function myFunction5(){
+    document.getElementById("demo").innerHTML += "Moused out! <br>";
+}
+
+//"Resize" event handler
+window.addEventListener("resize", function(){
+    this.document.getElementById("demo2").innerHTML = Math.random();
+}) 
+
